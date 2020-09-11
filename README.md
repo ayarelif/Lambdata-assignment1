6e8474e10a406aaf886f7f6b39fee75c3c0206d1
Elif Ayar <turkiyem_istanbul@hotmail.com>
2020-09-11 00:06:22 -0500

    Add the Read.me

--- README.md
+++ README.md
@@ -5,18 +5,11 @@ Collection of data Science for first assignment.
 
 pip install -i https://test.pypi.org/simple/ Lambdata-assignment1
 
+
+
 ## Usage
 
-TODO
-
 '''
-from my_lambdata.first_function import my_function
-from my_lambdata.first_function import my_int
+I create the three functions under the folder of my_lambdata and one class. In order to check my docstring I use pep 8 [demands] ("http://pep8online.com/")
 
-name = ["Elif", "Vera", "Musa","Harun","Ekmel","Huma"]
-int_rate="14.74%"
-
-if __name__ == '__main__':
-    print(my_function(name))
-    print(my_int(int_rate))
 '''
--- test/assignment_test.py
+++ test/assignment_test.py
@@ -8,12 +8,23 @@ df=DataFrame({"abbrev":["EA","VA","MA","HA","EAY","HAY"]})
 class TestMyAssignment(unittest.TestCase):
   
     def test_assignment(self):
-        #self.assertEqual('foo'.upper(), 'FOO')
-        self.assertEqual(add_names_column(df).iloc[[0],[1]], "Elif Ayar") 
+        df=DataFrame({"abbrev":["EA","VA","MA","HA","EAY","HAY"]})
+
+        # make sure our test is setup properly
+
+        self.assertEqual(len(df.columns), 1) 
+        self.assertEqual(list(df.columns),['abbrev']) 
+        self.assertEqual(df.iloc[0]["abbrev"],'EA') 
+
+        # what code can we write, referencing df to know if our function
+        # did what it was supposed to do
+        mapped_df=add_names_column(df)
+
+        self.assertEqual(len(mapped_df.columns), 2) 
+        self.assertEqual(list(mapped_df.columns),['abbrev','name']) 
+        self.assertEqual(mapped_df.iloc[0]["abbrev"],'EA') 
+        self.assertEqual(mapped_df.iloc[0]["name"],'Elif Ayar') 
 
 if __name__ == '__main__':
     unittest.main()
-
-#df=DataFrame({"abbrev":["EA","VA","MA","HA","EAY","HAY"]})
-#mapped_df=add_names_column(df)
 
